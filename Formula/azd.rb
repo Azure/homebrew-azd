c9191c3a67fbfc452d5139115a6b2b2c178e0c15
Daniel Jurek <djurek@microsoft.com>
2023-03-17 11:37:55 -0700

    Set caveats in install formula

--- Formula/azd.rb
+++ Formula/azd.rb
@@ -6,13 +6,25 @@ class Azd < Formula
     sha256 "ff1736950c02d67d30a0a9b714843a6e0d5dd936c1e071320fdd6335d5b63666"
     license "MIT"
 
+    def caveats
+        s = <<~EOS
+            The #{desc} collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
+            You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
+            
+            Read more about #{desc} telemetry: https://github.com/Azure/azure-dev#data-collection
+        EOS
+        on_arm do
+            s += <<~EOS
+                The #{desc} is built for Intel macOS and so requires Rosetta 2 to be installed.
+                You can install Rosetta 2 with:
+                  softwareupdate --install-rosetta
+                Note that it is very difficult to remove Rosetta 2 once it is installed.
+            EOS
+        end
+    end
+
     def install
         bin.install "azd-darwin-amd64" => "azd"
-
-        ohai "The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience."
-        ohai "You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use."
-        ohai ""
-        ohai "Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection"
     end
 
     test do
