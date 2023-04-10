f28433d6a2c1a6efd21e706e4d73bd990d46f25b
azure-sdk <azuresdk@microsoft.com>
2023-04-10 21:02:06 +0000

    Update formula for azd release 0.8.0-beta.1

--- Formula/azd.rb
+++ Formula/azd.rb
@@ -1,24 +1,44 @@
 class Azd < Formula
-    desc "Azure Developer CLI"
-    homepage "https://github.com/azure/azure-dev"
-    url "https://github.com/Azure/azure-dev/releases/download/azure-dev-cli_0.7.0-beta.1/azd-darwin-amd64.zip"
-    version "0.7.0-beta.1"
-    sha256 "ff1736950c02d67d30a0a9b714843a6e0d5dd936c1e071320fdd6335d5b63666"
-    license "MIT"
+  desc "Azure Developer CLI"
+  homepage "https://github.com/azure/azure-dev"
+  url "https://github.com/Azure/azure-dev/releases/download/azure-dev-cli_0.8.0-beta.1/azd-darwin-amd64.zip"
+  version "0.8.0-beta.1"
+  sha256 "8dfd228e7981f2f82e0de013ed6667e02a6cbb3ddedee784ad78f90633a29f05"
+  license "MIT"
 
-    def install
-        bin.install "azd-darwin-amd64" => "azd"
+  def install
+    bin.install "azd-darwin-amd64" => "azd"
+  end
 
-        ohai "The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience."
-        ohai "You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use."
-        ohai ""
-        ohai "Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection"
+  def caveats
+    caveat = <<~EOS
+      The #{desc} collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
+      You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
+
+      Read more about #{desc} telemetry: https://github.com/Azure/azure-dev#data-collection
+
+      azd may download binaries to ~/.azd/bin and, depending on how azd was used on this machine,
+      may download binaries to other users' home directories in their .azd/bin directory.
+      These binaries will need to be removed manually upon uninstall.
+      To remove such binaries from your home directory, run 'rm -rf ~/.azd/bin'.
+
+    EOS
+    on_arm do
+      caveat += <<~EOS
+
+        The #{desc} is built for Intel macOS and so requires Rosetta 2 to be installed.
+        You can install Rosetta 2 with:
+          softwareupdate --install-rosetta
+        Note that it is very difficult to remove Rosetta 2 once it is installed.
+      EOS
     end
+    caveat
+  end
 
-    test do
-        version_output = shell_output "#{bin}/azd version"
-        assert_equal 0, $CHILD_STATUS.exitstatus
-        assert_match "azd version", version_output
-    end
+  test do
+    version_output = shell_output "#{bin}/azd version"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+    assert_match "azd version", version_output
+  end
 end
 
