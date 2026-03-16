f63e9fb8082ce6ffa4e794d1ded00f46103e7e7c
azure-sdk <azuresdk@microsoft.com>
2026-03-16 17:02:58 +0000

    Update for azd release 1.24.0-beta.1-daily.6016065

---
+++ Casks/azd@daily.rb
@@ -0,0 +1,50 @@
+cask "azd@daily" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.24.0-beta.1-daily.6016065"
+
+  sha256 arm: "e6cd8666baae7fcbada8148acd6ecef899b93bd84465b7f8a6746ce140a5fc69",
+         intel: "3502d74116f4740dd1daeb64c5980ebb05db3dbc148f87bfbf6310867585af67",
+         arm64_linux: "ededd8b636ed1416948be4558d303f5e964458229208f609b98fab75dd668727",
+         x86_64_linux: "bfcc6c724d250324cbf0eb259aac919ccffeda457140b66435729f02cd9679d7"
+
+  # File extension differs between mac (.zip) and linux (.tar.gz)
+  on_macos do
+    # Daily releases are staged in a different storage location to keep the repo
+    # releases focused on supported releases.
+    url "https://azuresdkartifacts.z5.web.core.windows.net/azd/standalone/daily/archive/#{version}/azd-darwin-#{arch}.zip"
+    binary "azd-darwin-#{arch}", target: "azd"
+  end
+
+  # File extension differs between mac (.zip) and linux (.tar.gz)
+  on_linux do
+    # Daily releases are staged in a different storage location to keep the repo
+    # releases focused on supported releases.
+    url "https://azuresdkartifacts.z5.web.core.windows.net/azd/standalone/daily/archive/#{version}/azd-linux-#{arch}.tar.gz"
+    binary "azd-linux-#{arch}", target: "azd"
+  end
+
+  name "Azure Developer CLI (Daily)"
+  desc "Azure Developer CLI"
+  homepage "https://github.com/azure/azure-dev"
+
+  conflicts_with cask: "azd"
+
+  postflight do
+    # Set .installed-by.txt to "brew" to configure azd behavior for homebrew-managed installs
+    File.write("#{staged_path}/.installed-by.txt", "brew")
+  end
+
+  caveats <<~EOS
+    The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
+    You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
+
+    Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection
+
+    azd may download binaries to ~/.azd/bin and, depending on how azd was used on this machine,
+    may download binaries to other users' home directories in their .azd/bin directory.
+    These binaries will need to be removed manually upon uninstall.
+    To remove such binaries from your home directory, run 'rm -rf ~/.azd/bin'.
+  EOS
+end
+
