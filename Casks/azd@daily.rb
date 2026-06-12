9584367b2005ab76299612d68ccebf99f01aae30
azure-sdk <azuresdk@microsoft.com>
2026-06-12 22:14:14 +0000

    Update for azd release 1.26.0-beta.1-daily.6430898

--- Casks/azd@daily.rb
+++ Casks/azd@daily.rb
@@ -1,12 +1,12 @@
 cask "azd@daily" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.26.0-beta.1-daily.6426317"
+  version "1.26.0-beta.1-daily.6430898"
 
-  sha256 arm: "eaf0d0b385cde64aad4c9a98de7735bed5de2310b485b418028f9d2944cfeff8",
-         intel: "d1fb7658f069fe1626a0c17c226ac4db87e7a0dae551316caf134c2c4c8d2898",
-         arm64_linux: "6ee87e83ba380e0b8f407f83f99d64bbaef508c4c39e45475571ba76efb7b72d",
-         x86_64_linux: "b6fd56d3f76cca7a33b82de458c00f7c579333a035f18d696c62b298aaa719fc"
+  sha256 arm: "e5abb9d31e4d1ca4b674ca6628cad274f29f57a3e655b869f8139118af16c628",
+         intel: "5298f2060326ff0aee4f7c80ad68cd68c6a7c520f713ed6e90a582f04c8385cd",
+         arm64_linux: "c35dc720cf1d2a4c6c3fd0e8c7e8254a4ae80a06c7b4f1e236ab70df128bf303",
+         x86_64_linux: "770f6e8388ceb91e5e71b550e505b6c44517e8e18ed7a5a4b39cdaf73576fab9"
 
   # File extension differs between mac (.zip) and linux (.tar.gz)
   on_macos do
